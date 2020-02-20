40ed6830aa41551d9340f6865d92e231f280121a
eivinddh <eivind.d.halderaker@hotmail.com>
2020-02-20 13:43:13 +0100

    Format code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Login from './Pages/Login'
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Pages/Login";
 
 export default function Routing() {
   return (
