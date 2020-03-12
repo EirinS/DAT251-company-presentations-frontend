3e80b0386e54de159ce7147ad94a3782ec75f5c5
Even Sandvik <evensandvik207@gmail.com>
2020-03-12 15:23:53 +0100

    cardActions added on cards

--- src/Components/CardsComponent.jsx
+++ src/Components/CardsComponent.jsx
@@ -5,11 +5,17 @@ import render from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Typography from '@material-ui/core/Typography';
 import FormDialog from './PopupDialog'
+import Container from "@material-ui/core/Container";
+import Checkbox from "@material-ui/core/Checkbox";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
 
 
 const useStyles = makeStyles({
-  cardd: {
-    width: "25%",
+  card: {
+    width: "50%",
     height: "25%",
     marginBottom: 15,
     marginLeft: 15,
@@ -23,7 +29,7 @@ const useStyles = makeStyles({
   flexWrap: "wrap"
   },
   title: {
-    marginLeft: 5,
+    marginLeft: 0,
     marginTop: 5,
   },
   pos: {
@@ -104,17 +110,32 @@ export default function CardComponent() {
   Object.keys(bedpressJSONObject.bedpress).forEach(index => {
     let card = bedpressJSONObject.bedpress[index];
     cardList.push(
-      <Card className={classes.cardd}>
-       <Typography className={classes.title} color="textSecondary" align="left" gutterBottom>
-        {card.dato}
-       </Typography>
-       <Typography variant="h5" component="h2">
-          {card.bedriftsnavn}
-        </Typography>
-       <Typography className={classes.pos} color="textSecondary">
-          {card.info}
-        </Typography>
-       <FormDialog prop={card}/>
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+              component="img"
+              height="140"
+              className={classes.media}
+              image="https://www.bindeleddet.no/uploads/companies/1082/logo/medium_LOGO-BEKKlogo.png"
+              title="Bekk"
+        />
+        <CardContent>
+         <Typography className={classes.title} color="textSecondary" align="left" gutterBottom>
+          {card.dato}
+         </Typography>
+         <Typography variant="h5" component="h2">
+            {card.bedriftsnavn}
+          </Typography>
+         <Typography className={classes.pos} color="textSecondary">
+            {card.info}
+          </Typography>
+
+        </CardContent>
+        </CardActionArea>
+        <CardActions>
+        <Grid container spacing={12}>
+          <FormDialog prop={card} /></Grid>
+        </CardActions>
       </Card>
     );
   });
