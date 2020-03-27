0f5a45b2f444057b3d93ee6b28fa0f393881408b
Even Sandvik <evensandvik207@gmail.com>
2020-03-27 21:40:49 +0100

    init

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 ## Backend repository
 https://github.com/EirinS/DAT251-company-presentation-backend
+
+## Setup frontend
+
+In terminal write
+1. npm install
+2. npm start
+3. to see page go to the urls /dashboard or /login
+
+##Note
+Always use npm for this project, avoid others like yarn
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,9 +3,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import CardComponent from "../Components/CardsComponent";
 import DropdownMenu from "../Components/DropdownComponent";
+import {AxiosInstance as axios} from "axios";
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
@@ -16,7 +17,51 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function Dashboard() {
+  const [presentations, setPresentations] = useState(null);
   const classes = useStyles();
+
+  //Runs when page is loaded
+  useEffect(() => {
+    getPresentations().then();
+  }, []);
+
+
+  //fetches all presentations from backend
+  //TODO: not working
+  /*const getPresentations = async () => {
+    console.log("is fetching");
+    const response = await fetch("http://localhost:8080/allPresentations");
+    const data = await response.json();
+    console.log(data);
+    setPresentations(data);
+    console.log(presentations);
+  };
+
+  const getPresentations2 = async () => {
+    fetch("http://localhost:8080/allPresentations")
+        .then(res => res.json())
+        .then(
+            (result) => {
+              setPresentations(result);
+            },
+            (error) => {
+              setPresentations([]);
+            }
+        )
+  };*/
+
+  //Uses axios instead
+  const getPresentations = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/allPresentations');
+      console.log(response.data);
+      setPresentations(response.data);
+    }
+    catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div className={classes.root}>
       <div>
@@ -26,6 +71,7 @@ export default function Dashboard() {
             <Typography variant="h6" color="inherit">
               Dashboard
             </Typography>
+            {console.log(presentations)}
           </Toolbar>
         </AppBar>
       </div>
